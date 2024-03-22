3bbb56384ab598e0124e887b038022b9ba786bd6
Sébastien Lotten <129305476+SHACRAM@users.noreply.github.com>
2024-03-22 11:53:38 +0100

    initial commit

---
+++ src/api/map-place/controllers/map-place.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * map-place controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::map-place.map-place');
---
+++ src/api/map-place/routes/map-place.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * map-place router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::map-place.map-place');
